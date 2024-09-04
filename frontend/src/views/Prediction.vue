93a36232744c2838ec95d418dd1f073715053f47
MichaelLevin5908 <michaellevin42@g.ucla.edu>
2024-09-03 19:11:22 -0700

    frontend fixes

--- frontend/src/views/Prediction.vue
+++ frontend/src/views/Prediction.vue
@@ -1,139 +1,169 @@
 <template>
-  <div
-    class="d-flex flex-column justify-content-center align-items-center"
-    style="height: 80vh"
-  >
-    <div class="p-2">
+  <div class="container-fluid d-flex flex-column justify-content-center align-items-center" style="min-height: 100vh;">
+    <div class="text-center mb-4">
       <h1 v-if="!APIResult.length">Enter Housing Data to Make Prediction</h1>
       <h1 v-else style="font-size:4rem">{{ APIResult }}</h1>
     </div>
-    <div class="p-2">
-      <form @submit.prevent>
-        <div class="form-row" style="max-width:500px">
-          <div class="form-group col-md-4">
-            <label class="col-5 px-0" for="CRIM">CRIM</label>
+    <div class="card p-4 shadow-sm" style="width: 100%; max-width: 700px;">
+      <form @submit.prevent="predict">
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="CRIM">CRIM</label>
             <input
-              class="form-control"
+              class="form-control form-control-lg"
               v-model.trim="housingData.CRIM"
               type="number"
               id="CRIM"
+              placeholder="Crime rate"
+              required
             />
           </div>
-          <div class="form-group col-md-4">
-            <label class="col-5 px-0" for="ZN">ZN</label>
+          <div class="form-group col-md-6">
+            <label for="ZN">ZN</label>
             <input
-              class="form-control"
+              class="form-control form-control-lg"
               v-model.trim="housingData.ZN"
               type="number"
               id="ZN"
+              placeholder="Zoning"
+              required
             />
           </div>
-          <div class="form-group col-md-4">
-            <label class="col-5 px-0" for="INDUS">INDUS</label>
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="INDUS">INDUS</label>
             <input
-              class="form-control"
+              class="form-control form-control-lg"
               v-model.trim="housingData.INDUS"
               type="number"
               id="INDUS"
+              placeholder="Industry"
+              required
             />
           </div>
-          <div class="form-group col-md-4">
-            <label class="col-5 px-0" for="CHAS">CHAS</label>
+          <div class="form-group col-md-6">
+            <label for="CHAS">CHAS</label>
             <input
-              class="form-control"
+              class="form-control form-control-lg"
               v-model.trim="housingData.CHAS"
               type="number"
               id="CHAS"
+              placeholder="Charles River dummy variable"
+              required
             />
           </div>
-          <div class="form-group col-md-4">
-            <label class="col-5 px-0" for="NOX">NOX</label>
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="NOX">NOX</label>
             <input
-              class="form-control"
+              class="form-control form-control-lg"
               v-model.trim="housingData.NOX"
               type="number"
               id="NOX"
+              placeholder="Nitric oxides concentration"
+              required
             />
           </div>
-          <div class="form-group col-md-4">
-            <label class="col-5 px-0" for="RM">RM</label>
+          <div class="form-group col-md-6">
+            <label for="RM">RM</label>
             <input
-              class="form-control"
+              class="form-control form-control-lg"
               v-model.trim="housingData.RM"
               type="number"
               id="RM"
+              placeholder="Average number of rooms"
+              required
             />
           </div>
-          <div class="form-group col-md-4">
-            <label class="col-5 px-0" for="AGE">AGE</label>
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="AGE">AGE</label>
             <input
-              class="form-control"
+              class="form-control form-control-lg"
               v-model.trim="housingData.AGE"
               type="number"
               id="AGE"
+              placeholder="Age of buildings"
+              required
             />
           </div>
-          <div class="form-group col-md-4">
-            <label class="col-5 px-0" for="DIS">DIS</label>
+          <div class="form-group col-md-6">
+            <label for="DIS">DIS</label>
             <input
-              class="form-control"
+              class="form-control form-control-lg"
               v-model.trim="housingData.DIS"
               type="number"
               id="DIS"
+              placeholder="Distance to employment centers"
+              required
             />
           </div>
-          <div class="form-group col-md-4">
-            <label class="col-5 px-0" for="RAD">RAD</label>
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="RAD">RAD</label>
             <input
-              class="form-control"
+              class="form-control form-control-lg"
               v-model.trim="housingData.RAD"
               type="number"
               id="RAD"
+              placeholder="Index of accessibility to highways"
+              required
             />
           </div>
-          <div class="form-group col-md-4">
-            <label class="col-5 px-0" for="TAX">TAX</label>
+          <div class="form-group col-md-6">
+            <label for="TAX">TAX</label>
             <input
-              class="form-control"
+              class="form-control form-control-lg"
               v-model.trim="housingData.TAX"
               type="number"
               id="TAX"
+              placeholder="Property tax rate"
+              required
             />
           </div>
-          <div class="form-group col-md-4">
-            <label class="col-5 px-0" for="PTRATIO">PTRATIO</label>
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="PTRATIO">PTRATIO</label>
             <input
-              class="form-control"
+              class="form-control form-control-lg"
               v-model.trim="housingData.PTRATIO"
               type="number"
               id="PTRATIO"
+              placeholder="Pupil-teacher ratio"
+              required
             />
           </div>
-          <div class="form-group col-md-4">
-            <label class="col-5 px-0" for="B">B</label>
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-12">
+            <label for="LSTAT">LSTAT</label>
             <input
-              class="form-control"
-              v-model.trim="housingData.B"
-              type="number"
-              id="B"
-            />
-          </div>
-          <div class="form-group col-md-4">
-            <label class="col-5 px-0" for="LSTAT">LSTAT</label>
-            <input
-              class="form-control"
+              class="form-control form-control-lg"
               v-model.trim="housingData.LSTAT"
               type="number"
               id="LSTAT"
+              placeholder="Lower status of the population"
+              required
             />
           </div>
-          <button
-            @click="predict"
-            type="button"
-            class="btn btn-primary btn-lg btn-block"
-          >
+        </div>
+        <div class="text-center">
+          <button type="submit" class="btn btn-primary btn-lg btn-block mt-4 btn-submit">
             Submit
           </button>
+        </div>
+        <div v-if="loading" class="text-center mt-3">
+          <div class="spinner-border text-primary" role="status">
+            <span class="sr-only">Loading...</span>
+          </div>
+        </div>
+        <div v-if="errorMessage" class="alert alert-danger mt-3" role="alert">
+          {{ errorMessage }}
         </div>
       </form>
     </div>
@@ -142,6 +172,7 @@
 
 <script>
 import { getAPI } from "@/axios";
+
 export default {
   name: "PredictionPosts",
   data() {
@@ -158,13 +189,18 @@ export default {
         RAD: "",
         TAX: "",
         PTRATIO: "",
+        
         LSTAT: ""
       },
-      APIResult: []
+      APIResult: [],
+      loading: false,
+      errorMessage: null,
     };
   },
   methods: {
     predict() {
+      this.loading = true;
+      this.errorMessage = null;
       getAPI
         .get("/predict", {
           params: {
@@ -183,12 +219,15 @@ export default {
           }
         })
         .then(response => {
-          console.log("Received data successfully");
           this.APIResult = response.data;
           console.log(response.data);
         })
         .catch(err => {
-          console.log(err);
+          this.errorMessage = "Failed to fetch prediction. Please try again.";
+          console.error(err);
+        })
+        .finally(() => {
+          this.loading = false;
         });
     }
   }
@@ -196,5 +235,57 @@ export default {
 </script>
 
 <style>
-/* Add your custom styles here */
+/* Custom styles */
+body {
+  background-color: #f8f9fa;
+  position: center;
+}
+
+.card {
+  border-radius: 10px;
+  border: none;
+}
+
+h1 {
+  font-weight: 300;
+  color: #343a40;
+}
+
+.form-control {
+  border-radius: 0.375rem;
+  border: 1px solid #ced4da;
+}
+
+.form-control-lg {
+  height: calc(1.5em + 1rem + 2px);
+  font-size: 1.25rem;
+  line-height: 1.5;
+}
+
+.btn-primary {
+  background-color: #007bff;
+  border-color: #007bff;
+}
+
+.btn-primary:hover {
+  background-color: #0056b3;
+  border-color: #004085;
+}
+
+.spinner-border {
+  width: 3rem;
+  height: 3rem;
+}
+
+.alert-danger {
+  border-radius: 0.375rem;
+
+
+.btn-submit {
+  padding: 1rem 2rem; 
+  font-size: 1.5rem; 
+} 
+
+}
 </style>
+
